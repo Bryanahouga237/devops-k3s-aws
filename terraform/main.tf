478a251fed02319fa10e3371a3b1f3a5229e0a8e
Bryanahouga237 <bryanahouga@gmail.com>
2026-04-23 17:15:31 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
-# 1. On enregistre ta clé publique sur AWS
+# 1. clé publique sur AWS
 resource "aws_key_pair" "k3s_key" {
   key_name   = "k3s-key"
   public_key = file("k3s-key.pub")
 }
 
-# 2. On crée le serveur EC2
+# 2.  serveur EC2
 resource "aws_instance" "k3s_server" {
   ami           = "ami-00c71bd4d220aa22a" # Ubuntu 22.04 LTS à Paris (eu-west-3)
-  instance_type = "t3.small"             # 2 vCPU, 4GB RAM comme recommandé
+  instance_type = "t3.small"             # 2 vCPU, 
 
   subnet_id              = aws_subnet.k3s_subnet.id
   vpc_security_group_ids = [aws_security_group.k3s_sg.id]
@@ -21,7 +21,7 @@ resource "aws_instance" "k3s_server" {
               # Installation de K3s (Version légère)
               curl -sfL https://get.k3s.io | sh -
               
-              # On ajuste les droits pour que kubectl soit accessible sans sudo
+              # kubectl soit accessible sans sudo
               chmod 644 /etc/rancher/k3s/k3s.yaml
               EOF
   tags = {
@@ -29,4 +29,3 @@ resource "aws_instance" "k3s_server" {
   } 
 }
 
-# mon adresse ip publique est: 35.180.24.145:32319
